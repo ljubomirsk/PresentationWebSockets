670dcaa193af7c8cc100f951611481dc7b5f559c
ENDAVA\lignov <Ljubomir.Ignov@endava.com>
2018-11-08 11:02:51 +0100

    Static files added.

---
+++ src/images/images.js
@@ -0,0 +1,7 @@
+import Slide1 from "./Slide1.PNG";
+import Slide2 from "./Slide2.PNG";
+
+export default {
+  Slide1,
+  Slide2
+};
---
+++ src/videos/videos.js
@@ -0,0 +1,7 @@
+import Video1 from "./gandalf.mp4";
+import Video2 from "./nyan_cat.mp4";
+
+export default {
+  Video1,
+  Video2
+};
